0da62cd65422d3ee8be5e53a5dd65d10f0e601b3
ivdekov <ivdekov@gmail.com>
2017-02-23 17:02:23 -0600

    Add circular overlays to map

--- NatureCallsApp/ViewController.swift
+++ NatureCallsApp/ViewController.swift
@@ -19,7 +19,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapView.delegate = self
         locationManager.delegate = self
+        testOverlay()
     }
     
     func setupLocationManager() {
@@ -27,9 +29,19 @@ class ViewController: UIViewController {
         centerMapOn(location: locationManager.location!)
     }
     
-    func centerMapOn( location: CLLocation) {
+    func centerMapOn(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func testOverlay() {
+        let mkCircle1 = MKCircle(center: locationManager.location!.coordinate, radius: CLLocationDistance(exactly: 1000.0)!)
+        mkCircle1.title = "circle1"
+        let mkCircle2 = MKCircle(center: locationManager.location!.coordinate, radius: CLLocationDistance(exactly: 500.0)!)
+        mkCircle2.title = "circle2"
+        let mkCircle3 = MKCircle(center: locationManager.location!.coordinate, radius: CLLocationDistance(exactly: 250.0)!)
+        mkCircle3.title = "circle3"
+        mapView.addOverlays([mkCircle1, mkCircle2, mkCircle3])
     }
     
 }
@@ -37,7 +49,7 @@ class ViewController: UIViewController {
 extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        centerMapOn(location: locations.last!)
+//        centerMapOn(location: locations.last!)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -53,6 +65,23 @@ extension ViewController: CLLocationManagerDelegate {
         default: break
         }
     }
-    
 }
 
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let circleOverlay = overlay as? MKCircle {
+            let mkCircleRenderer = MKCircleRenderer(circle: circleOverlay)
+            switch circleOverlay.title! {
+            case "circle1":
+                mkCircleRenderer.fillColor = UIColor(red: 1.0, green: 0, blue: 0, alpha: 0.1)
+            case "circle2":
+                mkCircleRenderer.fillColor = UIColor(red: 0, green: 1.0, blue: 0, alpha: 0.1)
+            case "circle3":
+                mkCircleRenderer.fillColor = UIColor(red: 0, green: 0, blue: 1.0, alpha: 0.1)
+            default: break
+            }
+            return mkCircleRenderer
+        }
+        return MKOverlayRenderer(overlay: overlay)
+    }
+}
